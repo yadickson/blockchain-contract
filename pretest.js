07d296f0d2a11a628dad0d9e5b38da69faeb7950
Yadickson Soto <yadickson@gmail.com>
2018-05-04 01:22:41 -0300

    Add truffle extern support

--- pretest.js
+++ pretest.js
@@ -1,12 +1,6 @@
 const lsof = require('lsof/lib/lsof');
 const kill = require('tree-kill');
 
-lsof.rawTcpPort(8555, function(data) {
-  if (data.length > 0) {
-    return kill(data[0].pid, 9);
-  }
-});
-
 lsof.rawTcpPort(8565, function(data) {
   if (data.length > 0) {
     return kill(data[0].pid, 9);
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 const testRPC = require("ethereumjs-testrpc");
 
-const devProvider = testRPC.provider({port: 8555});
+const devProvider = testRPC.provider({port: 8565});
 const prodProvider = new Web3.providers.HttpProvider("http://localhost:8545");
 
 module.exports = {
@@ -12,8 +12,7 @@ module.exports = {
     },
     production: {
       provider: prodProvider,
-      network_id: "*", // any network associated with your node
-      from: "0xeb8eb912ef81a7005fc1d7dd593abd907244e062"
+      network_id: "*"
     },
     coverage: {
       host: "localhost",
@@ -21,6 +20,11 @@ module.exports = {
       network_id: "*",
       gas: 0xfffffffffff,
       gasPrice: 0x01
+    },
+    truffle: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
     }
   }
 };
