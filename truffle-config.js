8a1e082740f160105b947e204c01665d4799796a
Yadickson Soto <yadickson.soto@basedos.cl>
2018-05-07 17:10:32 -0300

    Fix require didn't use

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,8 @@
 const Web3 = require('web3');
 const ganache = require("ganache-cli");
-const ganacheCore = require("ganache-core");
 
 const prodProvider = new Web3.providers.HttpProvider("http://localhost:8545");
 const devProvider = ganache.provider({port: 8555});
-const coverageProvider = ganacheCore.provider({port: 8555});
 
 module.exports = {
   networks: {
